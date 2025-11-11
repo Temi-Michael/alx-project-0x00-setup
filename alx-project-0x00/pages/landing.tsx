db2bcffe6df06b07786c10dd585a8d6a5d89ae1e
Temi Michael <olasehindemichael285@gmail.com>
2025-11-11 17:32:26 +0100

    Task Update : Implementing final touches for Button props

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,15 +5,17 @@ const Landing: React.FC = () => {
     const shapeClass = {
         small: 'rounded-sm',
         medium: 'rounded-md',
-        large: 'rounded-lg'
+        large: 'rounded-lg',
+        full: 'rounded-full'
     }
     return (
         <div>
             <h1 className=" text-xl font-extralight ">Landing Page</h1>
             <Card />
-            <Button title="Button 1" styles={ shapeClass.small} />
-            <Button title="Button 2" styles={ shapeClass.medium} />
-            <Button title="Button 3" styles={ shapeClass.large} />
+            <Button title="Button 1" styles={shapeClass.small} />
+            <Button title="Button 2" styles={shapeClass.medium} />
+            <Button title="Button 3" styles={shapeClass.large} />
+            <Button title="Button 3" styles={shapeClass.full} />
         </div>
     )
 }
